443ab71425139ca7b8ad2753ec526c1da693de7d
alvinabia <alvin.abia@icloud.com>
2017-10-04 18:56:13 -0400

    Added extra IconCard props and Title component for better reusability. Refactor compatible with contact.js

--- src/shared/components/iconCard/iconCard.js
+++ src/shared/components/iconCard/iconCard.js
@@ -2,25 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './iconCard.css';
 
+const Title = (props) => {
+  const { link, url, title, titleTag } = props;
+  const titleWrapper = link ? (<a href={url}>{title}</a>) : title;
+
+  switch (titleTag) {
+    case 'h3':
+      return (<h3 className={styles.cardTitle}>{titleWrapper}</h3>);
+    default:
+      return (<span className={styles.cardTitle}>{titleWrapper}</span>);
+  }
+};
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  titleTag: PropTypes.string.isRequired,
+  link: PropTypes.bool.isRequired,
+  url: PropTypes.string.isRequired
+};
+
 const IconCard = (props) => {
-  const { title, icon, iconPosition, url, text, links } = props;
-  const { card, cardIcon, cardTitle } = styles;
+  const { title, icon, titleTag, iconPosition, url, target, rel, text, textStyle, links } = props;
+  const { card, cardIcon } = styles;
 
   // Wrap the passed in icon/title/text props in anchor tags if a url was passed in
   // and bundle them into Icon/Title/Text components respectively..
   const iconWrapper = url && links.includes('icon') ? (<a href={url}>{icon}</a>) : icon;
   const Icon = () => (<div className={cardIcon}>{iconWrapper}</div>);
 
-  const titleWrapper = url && links.includes('title') ? (<a href={url}>{title}</a>) : title;
-  const Title = () => (<span className={cardTitle}>{titleWrapper}</span>);
-
   const textWrapper = url && links.includes('text') ? (<a href={url}>{text}</a>) : text;
-  const Text = () => (<p>{textWrapper}</p>);
+  const Text = () => (<p className={textStyle}>{textWrapper}</p>);
 
   return (
     <div className={card}>
       {iconPosition === 'top' ? <Icon /> : null}
-      <Title />
+      <Title
+        link={url && links.includes('title')}
+        title={title}
+        titleTag={titleTag}
+        url={url}
+        target={target}
+        rel={rel}
+      />
       {iconPosition === 'middle' ? <Icon /> : null}
       {text !== null ? <Text /> : null}
     </div>
@@ -30,18 +53,31 @@ const IconCard = (props) => {
 IconCard.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.element.isRequired,
-  titleTags: PropTypes.string,
-  iconPosition: PropTypes.string,
+  titleTag: PropTypes.oneOf(['h3', 'span']),
+  iconPosition: PropTypes.oneOf(['top', 'middle']),
   url: PropTypes.string,
-  text: PropTypes.string,
-  links: PropTypes.arrayOf(PropTypes.string)
+  target: PropTypes.string,
+  rel: PropTypes.string,
+  text: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element
+  ]),
+  textStyle: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.oneOf([
+    'title',
+    'icon',
+    'text'
+  ])),
 };
 
 IconCard.defaultProps = {
-  titleTags: 'span',
+  titleTag: 'span',
   iconPosition: 'top',
   url: null,
+  target: null,
+  rel: null,
   text: null,
+  textStyle: null,
   links: []
 };
 
